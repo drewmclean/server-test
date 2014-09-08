694f2d177022aa6f7734ba0e720d62f52c25c522
Drew Mclean <drew.mclean@effectiveui.com>
2014-09-08 11:42:34 -0600

    initial

---
+++ ServerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ServerTest
+//
+//  Created by Andrew Mclean on 9/5/14.
+//  Copyright (c) 2014 Spex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ServerTestTests/ServerTestTests.m
@@ -0,0 +1,50 @@
+//
+//  ServerTestTests.m
+//  ServerTestTests
+//
+//  Created by Andrew Mclean on 9/5/14.
+//  Copyright (c) 2014 Spex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ServerTestTests : XCTestCase
+
+@end
+
+@implementation ServerTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample1 {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testExample2 {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testExample3 {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
