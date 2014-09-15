080d6dbe73da7b8f126680b9cfd15f217ace8c6e
Drew Mclean <drew.mclean@effectiveui.com>
2014-09-15 11:14:26 -0600

    Increment build number (_version)

---
+++ tools/increment-version.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#
+# Use agvtool to set Xcode project build number and create git tag
+# Commit the updated .xcproject and plist files back to git
+# Note: requires Xcode project configured to use Apple Generic Versioning and git
+#
+# Usage: increment-version.sh
+#
+# modified version of: https://gist.github.com/rob-murray/8644974
+#
+
+NOTE="'increment-version.sh' will increment build version number of Xcode project"
+
+ 
+# just increment the version
+function next_build_version() {
+    # bump the version number
+    $agvtool next-version -all
+}
+ 
+function get_marketing_version() {
+    agvtool what-marketing-version -terse1
+}
+
+function get_build_version() {
+    agvtool what-version -terse 
+}
+
+# function git_tag(label, message)
+function git_tag() {
+ 
+    local _version="$1"
+ 
+    if [[ -z "$_version" ]]
+    then
+        echo "Param version is empty."
+        exit
+    fi
+
+    git tag -a $_version -m "Tag build $_version"
+    git push --tags
+
+    git add .
+    git commit -m "Increment build number (_version)"
+    git push
+}
+
+echo "$NOTE"
+ 
+#set_version $VERSION
+next_build_version
+ 
+# grab the marketing and build versions
+marketing_version=`get_marketing_version`
+new_version=`get_build_version`
+ 
+# will tag in this format; e.g. {marketing_version}.{build_version} 2.1.4.68
+version_id="$marketing_version.$new_version"
+
+# commit changes and tag
+git_tag $version_id
+ 
+echo "Set Xcode project to build version $new_version and tagged with label $version_id"
---
+++ tools/update-pods.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Run a pod update if we have a repo, otherwise run a pod install
+#
+
+#xcodebuild -project ./ServerTest.xcodeproj -target "ServerTest" -showBuildSettings
+
+if [ -e "Pods" ]
+then
+pod update
+else
+pod install
+fi
